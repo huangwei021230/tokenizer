81b4cc4e1d21275f6d84c558bc60ae27a1cb8568
huangwei021230 <1138608506@qq.com>
2024-04-05 13:16:29 +0800

    feat: tranfer to ubuntu

--- myImep/main.cpp
+++ myImep/main.cpp
@@ -4,8 +4,7 @@
 #include "tokenization.h"
 
 int main(){
-//    tokenizer::GPT2Tokenizer tokenizer(L"../vocab.json", L"../merges.txt", 1024, 0, L"<unk>");
-    tokenizer::AutoTokenizer autoTokenizer("../tokenizer.json");
+    tokenizer::GPT2Tokenizer tokenizer(L"../vocab.json", L"../merges.txt", 1024, 0, L"<unk>");
     std::wstring text = L"Hello, my dog is cute";
 //    std::vector<std::wstring> tokens = tokenizer.tokenize(text);
 //    for (const auto &token : tokens) {
--- myImep/tokenization.cpp
+++ myImep/tokenization.cpp
@@ -12,11 +12,9 @@
 #include <memory>
 #include <algorithm>
 #include <map>
-#include <tokenizers_cpp.h>
 #include <locale>
 #include <codecvt>
 #define ll long long
-using tokenizers::Tokenizer;
 
 namespace tokenizer{
     const std::unordered_map<std::wstring, std::wstring> VOCAB_FILES_NAMES = {
@@ -121,76 +119,7 @@ namespace tokenizer{
         return vocab;
     }
 
-    std::string LoadBytesFromFile(const std::string& path) {
-        std::ifstream fs(path, std::ios::in | std::ios::binary);
-        if (fs.fail()) {
-            std::cerr << "Cannot open " << path << std::endl;
-            exit(1);
-        }
-        std::string data;
-        fs.seekg(0, std::ios::end);
-        size_t size = static_cast<size_t>(fs.tellg());
-        fs.seekg(0, std::ios::beg);
-        data.resize(size);
-        fs.read(data.data(), size);
-        return data;
-    }
 
-    void PrintEncodeResult(const std::vector<int>& ids) {
-        std::cout << "tokens=[";
-        for (size_t i = 0; i < ids.size(); ++i) {
-            if (i != 0) std::cout << ", ";
-            std::cout << ids[i];
-        }
-        std::cout << "]" << std::endl;
-    }
-
-    void TestTokenizer(std::unique_ptr<Tokenizer> tok, bool print_vocab = false,
-                       bool check_id_back = true) {
-        // Check #1. Encode and Decode
-        std::string prompt = "What is the  capital of Canada?";
-        std::vector<int> ids = tok->Encode(prompt);
-        std::string decoded_prompt = tok->Decode(ids);
-        PrintEncodeResult(ids);
-        std::cout << "decode=\"" << decoded_prompt << "\"" << std::endl;
-        assert(decoded_prompt == prompt);
-
-        // Check #2. IdToToken and TokenToId
-        std::vector<int32_t> ids_to_test = {0, 1, 2, 3, 32, 33, 34, 130, 131, 1000};
-        for (auto id : ids_to_test) {
-            auto token = tok->IdToToken(id);
-            auto id_new = tok->TokenToId(token);
-            std::cout << "id=" << id << ", token=\"" << token << "\", id_new=" << id_new << std::endl;
-            if (check_id_back) {
-                assert(id == id_new);
-            }
-        }
-
-        // Check #3. GetVocabSize
-        auto vocab_size = tok->GetVocabSize();
-        std::cout << "vocab_size=" << vocab_size << std::endl;
-
-        std::cout << std::endl;
-    }
-
-    AutoTokenizer::AutoTokenizer(const std::string &tokenizer_json_file){
-        std::cout << "Tokenizer: Huggingface" << std::endl;
-
-        auto start = std::chrono::high_resolution_clock::now();
-
-        // Read blob from file.
-        auto blob = LoadBytesFromFile(tokenizer_json_file);
-        // Note: all the current factory APIs takes in-memory blob as input.
-        // This gives some flexibility on how these blobs can be read.
-        auto tok = Tokenizer::FromBlobJSON(blob);
-
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-
-        std::cout << "Load time: " << duration << " ms" << std::endl;
-
-        TestTokenizer(std::move(tok), false, true);
-    }
 
     GPT2Tokenizer::GPT2Tokenizer(
             const std::wstring &vocab_file,
@@ -268,15 +197,14 @@ namespace tokenizer{
         }
         file_merges.close();
 
-        for(int i = 0;i<10;i++) {
-            std::cout << bpe_merges[i] << std::endl;
-        }
-
         // tuple
         std::vector<std::vector<std::string>> bpe_merge_words;
         for(const auto &line : bpe_merges) {
-            std::vector<std::string> merge = tokenizer::split(line);
-            bpe_merge_words.push_back(merge);
+            std::wstring wline = convertToUnicode(line);
+            std::vector<std::wstring> merge = tokenizer::split(wline);
+            for(auto & word : merge) {
+                std::wcout<< word << std::endl;
+            }
         }
 
         for(int i = 0;i< bpe_merge_words.size(); ++i) {
